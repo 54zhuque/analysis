d7e87c180a8e33bb3db2e88c5c9996f7114ff049
段豆豆 <duandoudou2011@gmail.com>
2018-05-23 14:13:48 +0800

    修改登录页面  form.submit()  问题

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,24 +1,24 @@
 layui.use(['form','layer','jquery'],function(){
     var form = layui.form,
-        layer = parent.layer === undefined ? layui.layer : top.layer
+        layer = parent.layer === undefined ? layui.layer : top.layer;
         $ = layui.jquery;
 
     //登录按钮
     form.on("submit(login)",function(data){
 
         $(this).text("登录中...").attr("disabled","disabled").addClass("layui-disabled");
-        form.submit();
+        $("#loginform").submit();
         return false;
-    })
+    });
 
     //表单输入效果
     $(".loginBody .input-item").click(function(e){
         e.stopPropagation();
         $(this).addClass("layui-input-focus").find(".layui-input").focus();
-    })
+    });
     $(".loginBody .layui-form-item .layui-input").focus(function(){
         $(this).parent().addClass("layui-input-focus");
-    })
+    });
     $(".loginBody .layui-form-item .layui-input").blur(function(){
         $(this).parent().removeClass("layui-input-focus");
         if($(this).val() != ''){
@@ -27,4 +27,4 @@ layui.use(['form','layer','jquery'],function(){
             $(this).parent().removeClass("layui-input-active");
         }
     })
-})
+});
