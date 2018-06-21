392727b2cee5d2c2c1c0766ac11ac50236deff3b
段豆豆 <duandoudou2011@gmail.com>
2018-06-21 14:10:47 +0800

    添加三好学生  评选

--- src/main/java/com/performance/analysis/controller/MainPageController.java
+++ src/main/java/com/performance/analysis/controller/MainPageController.java
@@ -5,12 +5,9 @@ import com.performance.analysis.pojo.EnglishEvaluation;
 import com.performance.analysis.pojo.MajorEvaluation;
 import com.performance.analysis.pojo.MoralEvaluation;
 import com.performance.analysis.pojo.PhysicalEvaluation;
-import com.performance.analysis.pojo.Student;
 import com.performance.analysis.service.StudentService;
 import com.performance.analysis.service.VariousGradeService;
 import java.util.List;
-
-import com.performance.analysis.service.impl.BuaTripleAResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,17 +37,28 @@ public class MainPageController {
     @Autowired
     private VariousGradeService variousGradeService;
 
+    /**
+     * 返回主页面
+     *
+     * @return 主页面
+     */
     @GetMapping("/main")
     public ModelAndView showMain() {
         ModelAndView view = new ModelAndView();
 
-        List<StudentEvaluationDto> ses=studentService.studentsResultOverview();
+        List<StudentEvaluationDto> ses = studentService.studentsResultOverview();
         view.addObject("students", ses);
 
         view.setViewName("index/main");
         return view;
     }
 
+    /**
+     * 成绩导入页面
+     *
+     * @param type 成绩类型
+     * @return 成绩导入页面
+     */
     @GetMapping("/import/{type}")
     public ModelAndView fileImport(@PathVariable String type) {
         ModelAndView view = new ModelAndView();
@@ -59,22 +67,32 @@ public class MainPageController {
         return view;
     }
 
+    @GetMapping("/bua/evaluations/{grade}/{subject}")
+    public ModelAndView showEvaluationPage(@PathVariable String grade, @PathVariable String subject) {
+        ModelAndView view = new ModelAndView();
+        view.addObject("grade", grade);
+        view.addObject("subject", subject);
+        view.setViewName("index/evaluations");
+        return view;
+    }
+
     /**
      * 上传文件页面数据封装
+     *
      * @param view
      * @param type
      */
-    private void setUploadViewData(ModelAndView view, String type){
-        if(ENGLISH.equals(type)){
+    private void setUploadViewData(ModelAndView view, String type) {
+        if (ENGLISH.equals(type)) {
             List<EnglishEvaluation> english = variousGradeService.listEnglishEvaluation();
             view.addObject("englishList", english);
-        }else if (MAJOY.equals(type)){
+        } else if (MAJOY.equals(type)) {
             List<MajorEvaluation> majoy = variousGradeService.listMajorEvaluation();
             view.addObject("majoyList", majoy);
-        }else if (MORAL.equals(type)){
+        } else if (MORAL.equals(type)) {
             List<MoralEvaluation> moral = variousGradeService.listMoralEvaluation();
             view.addObject("moralList", moral);
-        }else if (PHYSICAL.equals(type)){
+        } else if (PHYSICAL.equals(type)) {
             List<PhysicalEvaluation> physical = variousGradeService.listPhysicalEvaluation();
             view.addObject("physicalList", physical);
         }
